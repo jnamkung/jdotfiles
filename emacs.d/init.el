13a2f00b34a42ace3276d95ebf69d87fef5d0bbe
Chris Mealy <chrismealy@gmail.com>
2011-10-10 12:05:49 -0700

    moved emacs modes to a separate file

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -19,83 +19,24 @@
 (global-set-key (quote [27 down]) (quote scroll-up))
 (global-set-key (kbd "M-p") (lambda () (interactive) (scroll-down 1)))
 (global-set-key (kbd "M-n") (lambda () (interactive) (scroll-up 1)))
-
-;;;; Modes
-
-;; markdown
-(require 'markdown-mode)
-(add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
-
-;; slim
-(require 'slim-mode)
-
-;; yasnippet
-(load-lib-dir "yasnippet")
-(require 'yasnippet)
-(setq yas/root-directory (concat root-dir "yasnippet/snippets"))
-(yas/load-directory yas/root-directory)
-(yas/initialize)
-
-;; feature
-(load-lib-dir "feature-mode")
-(require 'feature-mode)
-(add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
-
-;; css
-(autoload 'css-mode "css-mode" "Mode for editing CSS files" t)
-(add-to-list 'auto-mode-alist '("\\.css$" . css-mode))
-(autoload 'rainbow-mode "rainbow-mode")
-
-;; ruby
-(autoload 'ruby-mode "ruby-mode" "Major mode for ruby files" t)
-(setq auto-mode-alist (append '(("\\.rb$"   . ruby-mode)
-                                ("\\.rake$"   . ruby-mode)
-                                ("\\.builder$" . ruby-mode)
-                                ("Rakefile"   . ruby-mode)
-                                ("Capfile"   . ruby-mode)
-                                ("Gemfile"   . ruby-mode)
-				) auto-mode-alist))
-
-;; rhtml
-(load-lib-dir "rhtml-mode")
-(require 'rhtml-mode)
-;; (add-hook 'rhtml-mode-hook (lambda () (rinari-launch)))
-
-;; yaml
-(require 'yaml-mode)
-(add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
-
-;; coffee
-(add-to-list 'load-path "~/.emacs.d/coffee-mode")
-(require 'coffee-mode)
-(add-to-list 'auto-mode-alist '("\\.coffee$" . coffee-mode))
-(add-to-list 'auto-mode-alist '("Cakefile" . coffee-mode))
-
-;; php
-(require 'php-mode)
-(add-hook 'php-mode-hook
-	  '(lambda () (define-abbrev php-mode-abbrev-table "ex" "extends")))
-(defun my-php-mode-common-hook ()
-  ;; my customizations for php-mode
-  (setq tab-width 4)
-  (setq c-basic-offset 4)
-  (c-set-offset 'topmost-intro-cont 4)
-  (c-set-offset 'class-open 0)
-  (c-set-offset 'inline-open 0)
-  (c-set-offset 'substatement-open 0)
-  (c-set-offset 'arglist-intro '+)
-  )
-
-;; io
-(autoload 'io-mode "io-mode" "Mode for editing Io files" t)
-(add-to-list 'auto-mode-alist '("\\.io$" . io-mode))
-
-;; haml
-(require 'haml-mode)
-(setq haml-mode-hook (function (lambda () (setq indent-tabs-mode nil))))
-
-
+(global-set-key (kbd "M-/") 'hippie-expand)
 
 ;; Save squiggle files somewhere out of the way
 (setq backup-directory-alist '(("." . "~/.saves")))
 
+(load-lib "modes")
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(font-lock-function-name-face ((((class color) (min-colors 88) (background light)) (:foreground "brightblue"))))
+ '(font-lock-keyword-face ((((class color) (min-colors 88) (background light)) (:foreground "Orange"))))
+ '(font-lock-string-face ((((class color) (min-colors 88) (background light)) (:foreground "green")))))
