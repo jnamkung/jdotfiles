66497308bb20c0903cf1adc184261ff12d524752
Ed Slocomb <edslocomb@gmail.com>
2014-12-16 14:05:14 -0800

    use new indent vars for web-mode

--- emacs.d/modes.el
+++ emacs.d/modes.el
@@ -95,7 +95,9 @@
 (add-to-list 'auto-mode-alist '("\\html.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\text.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
 
 ;; Yasnippet
 (require 'yasnippet)
